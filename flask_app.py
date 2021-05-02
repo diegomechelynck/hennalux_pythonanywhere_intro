995eae9f3107adadc1058b1f43a98dc0c61f4e46
Diego Mechelynck <diego.mechelynck@gmail.com>
2021-05-02 17:00:06 +0000

    cleaning some unnecssary comments

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,3 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, render_template
 from Predict_Next_Day_Weather import Predict_Next_Day_Weather, Get_Weather_History
 
@@ -8,8 +5,6 @@ app = Flask(__name__)
 
 @app.route("/temperature")
 def Show_Temp_Data():
-    #test=Get_Weather_History()
-    #resultlistofdict=[dict(r) for r in test]
     Temperature_History = Get_Weather_History()
     Temperature_Predictions = Predict_Next_Day_Weather()
 
